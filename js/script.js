c0d10f4c8b2c99045a838375a96d676a4c20ebaa
Andranik Torgomyan <andranik@nteams.com>
2023-10-24 13:26:15 +0400

    Fix

--- js/script.js
+++ js/script.js
@@ -8,5 +8,17 @@ const {
 
 AOS.init();
 
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 
+// ScrollTrigger.create({
+//     trigger: "#youll-get",
+//     start: `top top-=-79` ,
+//     end: `top top-=2500`,
+//     pin: true,
+//     onUpdate: function (e){
+//         const percent = e.progress * 100;
+//         console.log(percent)
+//
+//     }
+// });
