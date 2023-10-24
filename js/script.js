794d1ace46395c48889c30ac8ef2f7853ca7d1b0
armanpinachyan <pinachyan99@mail.ru>
2023-10-24 16:26:16 +0400

    16:26

--- js/script.js
+++ js/script.js
@@ -93,16 +93,16 @@ $('.slider').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
             }
         },
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToShow: 1,
+                slidesToScroll: 1,
             }
         },
         {
